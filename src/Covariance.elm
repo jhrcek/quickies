b0119b70497a31f0e595e693ec9fec04d991bcd4
Jan Hrček <honza.hrk@gmail.com>
2025-01-09 10:48:51 +0100

    Improve

--- src/Covariance.elm
+++ src/Covariance.elm
@@ -1,11 +1,10 @@
 module Covariance exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, input)
+import Html exposing (Html, button, div, input, text)
 import Html.Attributes as HA
 import Html.Events as HE
 import List exposing (sum)
-import Maybe
 import Random
 import Svg
 import Svg.Attributes as SA
@@ -19,12 +18,14 @@ type alias Model =
     }
 
 
-init : Model
-init =
-    { numPoints = 10
-    , points = []
-    , hoveredIndex = Nothing
-    }
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { numPoints = 10
+      , points = []
+      , hoveredIndex = Nothing
+      }
+    , generatePoints 10
+    )
 
 
 type Msg
@@ -33,10 +34,6 @@ type Msg
     | GotRandomPoints (List ( Float, Float ))
     | Hover Int
     | Unhover
-
-
-
--- UPDATE
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -57,24 +54,16 @@ update msg model =
                                 else
                                     Just n
                             )
-
-                -- We only allow 2..100
             in
             case maybeInt of
                 Just validN ->
                     ( { model | numPoints = validN }, Cmd.none )
 
                 Nothing ->
-                    -- If it's not a valid integer, or out of range, just ignore
                     ( model, Cmd.none )
 
         Generate ->
-            -- Generate random points within [0,1] × [0,1]
-            let
-                gen =
-                    Random.list model.numPoints (Random.pair (Random.float 0 1) (Random.float 0 1))
-            in
-            ( model, Random.generate GotRandomPoints gen )
+            ( model, generatePoints model.numPoints )
 
         GotRandomPoints newPoints ->
             ( { model | points = newPoints, hoveredIndex = Nothing }, Cmd.none )
@@ -86,17 +75,13 @@ update msg model =
             ( { model | hoveredIndex = Nothing }, Cmd.none )
 
 
-
--- SUBSCRIPTIONS
-
-
-subscriptions : Model -> Sub Msg
-subscriptions _ =
-    Sub.none
-
-
-
--- VIEW
+generatePoints : Int -> Cmd Msg
+generatePoints numPoints =
+    let
+        gen =
+            Random.list numPoints (Random.pair (Random.float 0 1) (Random.float 0 1))
+    in
+    Random.generate GotRandomPoints gen
 
 
 view : Model -> Html Msg
@@ -104,32 +89,35 @@ view model =
     div
         [ HA.style "width" "100vw"
         , HA.style "height" "100vh"
-        , HA.style "box-sizing" "border-box"
+        , HA.style "box-String.fromIntzing" "border-box"
         , HA.style "margin" "0"
         , HA.style "padding" "0"
         ]
-        [ -- Controls
-          div [ HA.style "padding" "1rem" ]
-            [ Html.text "Number of points (2..100): "
-            , input
-                [ HA.type_ "number"
-                , HA.value (String.fromInt model.numPoints)
-                , HA.min "2"
-                , HA.max "100"
-                , HE.onInput ChangeNumPoints
-                ]
-                []
-            , button [ HE.onClick Generate ] [ Html.text "Generate / Regenerate Random Points" ]
+        [ controlsView model
+        , viewGraph model
+        , viewExpandedFormula model
+        ]
+
+
+controlsView : Model -> Html Msg
+controlsView model =
+    div [ HA.style "padding" "1rem" ]
+        [ text "Number of points (2..100): "
+        , input
+            [ HA.type_ "number"
+            , HA.value (String.fromInt model.numPoints)
+            , HA.min "2"
+            , HA.max "100"
+            , HE.onInput ChangeNumPoints
             ]
-        , -- SVG Graph
-          viewGraph model
+            []
+        , button [ HE.onClick Generate ] [ text "Generate / Regenerate Random Points" ]
         ]
 
 
 viewGraph : Model -> Html Msg
 viewGraph model =
     let
-        -- Dimensions
         width =
             800
 
@@ -139,40 +127,90 @@ viewGraph model =
         margin =
             40
 
-        -- Convert data coordinate (0..1) to SVG coordinate
-        -- We consider y increasing upwards. However, in SVG, y grows downwards.
-        -- We'll invert the y-axis by subtracting from total height.
         scaleX x =
             margin + x * toFloat (width - 2 * margin)
 
         scaleY y =
             (toFloat height - margin) - y * toFloat (height - 2 * margin)
 
-        -- Averages
         meanX =
-            if List.length model.points == 0 then
+            if List.isEmpty model.points then
                 0
 
             else
-                List.map (\( x, _ ) -> x) model.points
-                    |> sum
-                    |> (\s -> s / toFloat (List.length model.points))
+                let
+                    totalX =
+                        List.map Tuple.first model.points |> sum
+                in
+                totalX / toFloat (List.length model.points)
 
         meanY =
-            if List.length model.points == 0 then
+            if List.isEmpty model.points then
                 0
 
             else
-                List.map (\( _, y ) -> y) model.points
-                    |> sum
-                    |> (\s -> s / toFloat (List.length model.points))
+                let
+                    totalY =
+                        List.map Tuple.second model.points |> sum
+                in
+                totalY / toFloat (List.length model.points)
 
-        -- Transform means
         meanXSvg =
             scaleX meanX
 
         meanYSvg =
             scaleY meanY
+
+        hoverRect =
+            case model.hoveredIndex of
+                Just i ->
+                    case List.drop i model.points |> List.head of
+                        Just ( px, py ) ->
+                            let
+                                pxSvg =
+                                    scaleX px
+
+                                pySvg =
+                                    scaleY py
+
+                                sign =
+                                    (px - meanX) * (py - meanY)
+
+                                fillColor =
+                                    if sign >= 0 then
+                                        "blue"
+
+                                    else
+                                        "red"
+
+                                rectX =
+                                    Basics.min pxSvg meanXSvg
+
+                                rectY =
+                                    Basics.min pySvg meanYSvg
+
+                                rectWidth =
+                                    abs (pxSvg - meanXSvg)
+
+                                rectHeight =
+                                    abs (pySvg - meanYSvg)
+                            in
+                            [ Svg.rect
+                                [ SA.x (String.fromFloat rectX)
+                                , SA.y (String.fromFloat rectY)
+                                , SA.width (String.fromFloat rectWidth)
+                                , SA.height (String.fromFloat rectHeight)
+                                , SA.fill fillColor
+                                , SA.fillOpacity "0.3"
+                                ]
+                                []
+                            ]
+
+                        Nothing ->
+                            []
+
+                Nothing ->
+                    []
     in
     Svg.svg
         [ SA.width (String.fromInt width)
@@ -180,100 +218,45 @@ viewGraph model =
         , SA.viewBox ("0 0 " ++ String.fromInt width ++ " " ++ String.fromInt height)
         , SA.style "background-color:white"
         ]
-        ([ -- X axis
-           Svg.line
-            [ SA.x1 (String.fromFloat <| toFloat margin)
-            , SA.y1 (String.fromFloat <| toFloat (height - margin))
-            , SA.x2 (String.fromFloat <| toFloat (width - margin))
-            , SA.y2 (String.fromFloat <| toFloat (height - margin))
+        ([ Svg.line
+            [ SA.x1 (String.fromInt margin)
+            , SA.y1 (String.fromInt (height - margin))
+            , SA.x2 (String.fromInt (width - margin))
+            , SA.y2 (String.fromInt (height - margin))
             , SA.stroke "black"
             , SA.strokeWidth "2"
             ]
             []
-         , -- Y axis
-           Svg.line
-            [ SA.x1 (String.fromFloat <| toFloat margin)
-            , SA.y1 (String.fromFloat <| toFloat margin)
-            , SA.x2 (String.fromFloat <| toFloat margin)
-            , SA.y2 (String.fromFloat <| toFloat (height - margin))
+         , Svg.line
+            [ SA.x1 (String.fromInt margin)
+            , SA.y1 (String.fromInt margin)
+            , SA.x2 (String.fromInt margin)
+            , SA.y2 (String.fromInt (height - margin))
             , SA.stroke "black"
             , SA.strokeWidth "2"
             ]
             []
-         , -- Mean X line
-           Svg.line
+         , Svg.line
             [ SA.x1 (String.fromFloat meanXSvg)
-            , SA.y1 (String.fromFloat <| toFloat margin)
+            , SA.y1 (String.fromInt margin)
             , SA.x2 (String.fromFloat meanXSvg)
-            , SA.y2 (String.fromFloat <| toFloat (height - margin))
+            , SA.y2 (String.fromInt (height - margin))
             , SA.stroke "grey"
             , SA.strokeDasharray "4,4"
             ]
             []
-         , -- Mean Y line
-           Svg.line
-            [ SA.x1 (String.fromFloat <| toFloat margin)
+         , Svg.line
+            [ SA.x1 (String.fromInt margin)
             , SA.y1 (String.fromFloat meanYSvg)
-            , SA.x2 (String.fromFloat <| toFloat (width - margin))
+            , SA.x2 (String.fromInt (width - margin))
             , SA.y2 (String.fromFloat meanYSvg)
             , SA.stroke "grey"
             , SA.strokeDasharray "4,4"
             ]
             []
          ]
-            ++ -- Hovered "square" (rectangle) from (meanX, meanY) to the hovered point
-               (case model.hoveredIndex of
-                    Just i ->
-                        case List.drop i model.points |> List.head of
-                            Just ( px, py ) ->
-                                let
-                                    pxSvg =
-                                        scaleX px
-
-                                    pySvg =
-                                        scaleY py
-
-                                    sign =
-                                        (px - meanX) * (py - meanY)
-
-                                    fillColor =
-                                        if sign >= 0 then
-                                            "blue"
-
-                                        else
-                                            "red"
-
-                                    rectX =
-                                        Basics.min pxSvg meanXSvg
-
-                                    rectY =
-                                        Basics.min pySvg meanYSvg
-
-                                    rectWidth =
-                                        abs (pxSvg - meanXSvg)
-
-                                    rectHeight =
-                                        abs (pySvg - meanYSvg)
-                                in
-                                [ Svg.rect
-                                    [ SA.x (String.fromFloat rectX)
-                                    , SA.y (String.fromFloat rectY)
-                                    , SA.width (String.fromFloat rectWidth)
-                                    , SA.height (String.fromFloat rectHeight)
-                                    , SA.fill fillColor
-                                    , SA.fillOpacity "0.3"
-                                    ]
-                                    []
-                                ]
-
-                            Nothing ->
-                                []
-
-                    Nothing ->
-                        []
-               )
-            ++ -- Points
-               List.indexedMap
+            ++ hoverRect
+            ++ List.indexedMap
                 (\i ( x, y ) ->
                     let
                         cx_ =
@@ -281,6 +264,13 @@ viewGraph model =
 
                         cy_ =
                             scaleY y
+
+                        titleText =
+                            "("
+                                ++ truncateFloat x
+                                ++ ", "
+                                ++ truncateFloat y
+                                ++ ")"
                     in
                     Svg.circle
                         [ SA.cx (String.fromFloat cx_)
@@ -290,17 +280,113 @@ viewGraph model =
                         , SE.onMouseOver (Hover i)
                         , SE.onMouseOut Unhover
                         ]
-                        []
+                        [ Svg.title []
+                            [ Svg.text titleText ]
+                        ]
                 )
                 model.points
         )
 
 
+{-| Show the expanded covariance formula and each summand (xᵢ - meanX)(yᵢ - meanY).
+Color them based on sign, and highlight the hovered item with a border.
+-}
+viewExpandedFormula : Model -> Html Msg
+viewExpandedFormula model =
+    let
+        meanX =
+            if List.isEmpty model.points then
+                0
+
+            else
+                sum (List.map Tuple.first model.points)
+                    / toFloat (List.length model.points)
+
+        meanY =
+            if List.isEmpty model.points then
+                0
+
+            else
+                sum (List.map Tuple.second model.points)
+                    / toFloat (List.length model.points)
+    in
+    div [ HA.style "margin" "1rem" ]
+        [ div [] [ text "Cov(X,Y) = Σ (xᵢ -  x̄ ) (yᵢ -  ȳ ), for i = 1..n" ]
+        , div []
+            (List.indexedMap (viewTerm meanX meanY model.hoveredIndex) model.points)
+        ]
+
+
+viewTerm :
+    Float
+    -> Float
+    -> Maybe Int
+    -> Int
+    -> ( Float, Float )
+    -> Html Msg
+viewTerm meanX meanY hoveredIndex i ( x, y ) =
+    let
+        dx =
+            x - meanX
+
+        dy =
+            y - meanY
+
+        product =
+            dx * dy
+
+        color =
+            if product >= 0 then
+                "blue"
+
+            else
+                "red"
+
+        border =
+            case hoveredIndex of
+                Just hI ->
+                    if hI == i then
+                        "2px solid black"
+
+                    else
+                        "none"
+
+                Nothing ->
+                    "none"
+
+        content =
+            "("
+                ++ truncateFloat dx
+                ++ ")·("
+                ++ truncateFloat dy
+                ++ ") = "
+                ++ truncateFloat product
+    in
+    div
+        [ HA.style "margin" "6px 0"
+        , HA.style "color" color
+        , HA.style "border" border
+        , HA.style "padding" "4px"
+        ]
+        [ text content ]
+
+
+{-| Helper to turn Float -> "0.12" with 2 decimals (or fewer if small)
+-}
+truncateFloat : Float -> String
+truncateFloat val =
+    let
+        rounded =
+            round (val * 100)
+    in
+    String.fromFloat (toFloat rounded / 100)
+
+
 main : Program () Model Msg
 main =
     Browser.element
-        { init = \_ -> ( init, Cmd.none )
+        { init = init
         , update = update
-        , subscriptions = subscriptions
+        , subscriptions = \_ -> Sub.none
         , view = view
         }
