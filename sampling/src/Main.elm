e5c0b3d4d5ae5e3ad1a2136bc5a7646b852a7d9d
Jan Hrček <honza.hrk@gmail.com>
2025-02-15 14:22:49 +0100

    Sampling 3 - simplify

--- sampling/src/Main.elm
+++ sampling/src/Main.elm
@@ -6,6 +6,7 @@ import Html exposing (Html)
 import Html.Attributes as HA
 import Html.Events as E
 import Random
+import Random.List
 import Set exposing (Set)
 
 
@@ -36,38 +37,61 @@ type alias Model =
     , sampledElements : List Int
     , elementColors : Dict Int String
     , nextColorIndex : Int
+    , batchSize : Int
     }
 
 
 repeatedElementColors : List String
 repeatedElementColors =
     [ "#ffcdd2" -- light red
-    , "#fff9c4" -- light yellow
-    , "#c8e6c9" -- light green
     , "#bbdefb" -- light blue
+    , "#c8e6c9" -- light green
     , "#e1bee7" -- light purple
-    , "#ffccbc" -- light orange
+    , "#ffe0b2" -- light orange
+    , "#b2dfdb" -- light teal
+    , "#f8bbd0" -- light pink
+    , "#d1c4e9" -- light indigo
+    , "#c5cae9" -- lighter blue
+    , "#dcedc8" -- lighter green
+    , "#fff9c4" -- light yellow
+    , "#d7ccc8" -- light brown
+    , "#cfd8dc" -- light blue grey
+    , "#f0f4c3" -- light lime
+    , "#b3e5fc" -- lighter sky blue
+    , "#ffccbc" -- lighter orange
+    , "#e0f7fa" -- lighter cyan
+    , "#f5f5f5" -- light grey
+    , "#e8eaf6" -- lightest indigo
+    , "#fce4ec" -- lightest pink
     ]
+
+
+defaultColor : String
+defaultColor =
+    -- light grey
+    "#e0e0e0"
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { samplingType = WithReplacement
-      , initialSetSize = 10
-      , availableElements = Set.fromList (List.range 1 10)
-      , sampledElements = []
-      , elementColors = Dict.empty
-      , nextColorIndex = 0
-      }
-    , Cmd.none
-    )
+    pure
+        { samplingType = WithReplacement
+        , initialSetSize = 10
+        , availableElements = Set.fromList (List.range 1 10)
+        , sampledElements = []
+        , elementColors = Dict.empty
+        , nextColorIndex = 0
+        , batchSize = 1
+        }
 
 
 type Msg
     = ChangeSamplingType SamplingType
     | ChangeInitialSetSize String
-    | SampleElement
-    | GotRandomElement Int
+    | ChangeBatchSize String
+    | SampleBatch
+    | GotSampleWithoutReplacement (List Int)
+    | GotSampleWithReplacement (List Int)
     | Reset
 
 
@@ -94,74 +118,96 @@ update msg model =
                             , sampledElements = []
                             , elementColors = Dict.empty
                             , nextColorIndex = 0
+                            , batchSize = min model.batchSize newSize
                         }
 
                 Nothing ->
                     pure model
 
-        SampleElement ->
+        ChangeBatchSize sizeStr ->
+            case String.toInt sizeStr of
+                Just size ->
+                    let
+                        maxBatchSize =
+                            case model.samplingType of
+                                WithReplacement ->
+                                    model.initialSetSize
+
+                                WithoutReplacement ->
+                                    Set.size model.availableElements
+                    in
+                    pure { model | batchSize = clamp 1 maxBatchSize size }
+
+                Nothing ->
+                    pure model
+
+        SampleBatch ->
             case model.samplingType of
                 WithReplacement ->
                     ( model
-                    , Random.generate GotRandomElement (Random.int 1 model.initialSetSize)
+                    , Random.generate GotSampleWithReplacement
+                        (Random.list model.batchSize (Random.int 1 model.initialSetSize))
                     )
 
                 WithoutReplacement ->
-                    if Set.isEmpty model.availableElements then
+                    if Set.size model.availableElements < model.batchSize then
                         pure model
 
                     else
-                        let
-                            asList =
-                                Set.toList model.availableElements
-                        in
                         ( model
-                        , Random.generate GotRandomElement
-                            (Random.int 0 (List.length asList - 1)
-                                |> Random.map (\idx -> Maybe.withDefault 0 (List.head (List.drop idx asList)))
-                            )
+                        , Random.List.choices model.batchSize (Set.toList model.availableElements)
+                            |> Random.map Tuple.first
+                            |> Random.generate GotSampleWithoutReplacement
                         )
 
-        GotRandomElement element ->
-            case model.samplingType of
-                WithReplacement ->
-                    let
-                        newElementColors =
-                            if
-                                not (Dict.member element model.elementColors)
-                                    && List.length (List.filter ((==) element) (element :: model.sampledElements))
-                                    > 1
-                            then
+        GotSampleWithReplacement elements ->
+            let
+                newSampledElements =
+                    model.sampledElements ++ elements
+
+                elementsToColor =
+                    newSampledElements
+                        |> List.filter
+                            (\n ->
+                                List.length (List.filter ((==) n) newSampledElements) > 1
+                            )
+                        |> Set.fromList
+                        |> Set.toList
+
+                newElementColors =
+                    List.foldl
+                        (\element colors ->
+                            if Dict.member element colors then
+                                colors
+
+                            else
                                 Dict.insert element
-                                    (List.drop (modBy (List.length repeatedElementColors) model.nextColorIndex) repeatedElementColors
+                                    (List.drop
+                                        (Dict.size colors
+                                            |> modBy (List.length repeatedElementColors)
+                                        )
+                                        repeatedElementColors
                                         |> List.head
-                                        |> Maybe.withDefault "#e0e0e0"
+                                        |> Maybe.withDefault defaultColor
                                     )
-                                    model.elementColors
+                                    colors
+                        )
+                        model.elementColors
+                        elementsToColor
+            in
+            pure
+                { model
+                    | sampledElements = newSampledElements
+                    , elementColors = newElementColors
+                    , nextColorIndex = Dict.size newElementColors
+                }
 
-                            else
-                                model.elementColors
-
-                        newNextColorIndex =
-                            if Dict.size newElementColors > Dict.size model.elementColors then
-                                model.nextColorIndex + 1
-
-                            else
-                                model.nextColorIndex
-                    in
-                    pure
-                        { model
-                            | sampledElements = model.sampledElements ++ [ element ]
-                            , elementColors = newElementColors
-                            , nextColorIndex = newNextColorIndex
-                        }
-
-                WithoutReplacement ->
-                    pure
-                        { model
-                            | availableElements = Set.remove element model.availableElements
-                            , sampledElements = model.sampledElements ++ [ element ]
-                        }
+        GotSampleWithoutReplacement elements ->
+            pure
+                { model
+                    | availableElements = List.foldl Set.remove model.availableElements elements
+                    , sampledElements = model.sampledElements ++ elements
+                }
 
         Reset ->
             pure
@@ -220,13 +266,38 @@ view model =
                 ]
             ]
         , Html.div [ HA.style "margin-bottom" "20px" ]
-            [ Html.button
-                [ E.onClick SampleElement
-                , HA.disabled (model.samplingType == WithoutReplacement && Set.isEmpty model.availableElements)
+            [ Html.text "Sample "
+            , Html.input
+                [ HA.type_ "number"
+                , HA.min "1"
+                , HA.max
+                    (String.fromInt
+                        (case model.samplingType of
+                            WithReplacement ->
+                                model.initialSetSize
+
+                            WithoutReplacement ->
+                                Set.size model.availableElements
+                        )
+                    )
+                , HA.value (String.fromInt model.batchSize)
+                , HA.style "width" "60px"
+                , E.onInput ChangeBatchSize
+                ]
+                []
+            , Html.text " elements "
+            , Html.button
+                [ E.onClick SampleBatch
+                , HA.disabled
+                    (model.samplingType
+                        == WithoutReplacement
+                        && Set.size model.availableElements
+                        < model.batchSize
+                    )
                 , HA.style "margin-right" "10px"
                 , HA.style "padding" "5px 10px"
                 ]
-                [ Html.text "Sample Element" ]
+                [ Html.text "go" ]
             , Html.button
                 [ E.onClick Reset
                 , HA.style "padding" "5px 10px"
@@ -256,7 +327,7 @@ viewSet elements =
                         [ HA.style "display" "inline-block"
                         , HA.style "margin" "5px"
                         , HA.style "padding" "5px 10px"
-                        , HA.style "background-color" "#e0e0e0"
+                        , HA.style "background-color" defaultColor
                         , HA.style "border-radius" "3px"
                         ]
                         [ Html.text (String.fromInt n) ]
@@ -266,25 +337,28 @@ viewSet elements =
 
 viewSampledList : SamplingType -> Dict Int String -> List Int -> Html msg
 viewSampledList samplingType elementColors elements =
-    Html.ul [ HA.style "list-style-type" "none", HA.style "padding" "10px" ]
-        (elements
-            |> List.map
-                (\n ->
-                    Html.li
-                        [ HA.style "display" "inline-block"
-                        , HA.style "margin" "5px"
-                        , HA.style "padding" "5px 10px"
-                        , HA.style "background-color"
-                            (case samplingType of
-                                WithReplacement ->
-                                    Dict.get n elementColors
-                                        |> Maybe.withDefault "#e0e0e0"
+    Html.ul
+        [ HA.style "list-style-type" "none"
+        , HA.style "padding" "10px"
+        ]
+        (List.map
+            (\n ->
+                Html.li
+                    [ HA.style "display" "inline-block"
+                    , HA.style "margin" "5px"
+                    , HA.style "padding" "5px 10px"
+                    , HA.style "background-color"
+                        (case samplingType of
+                            WithReplacement ->
+                                Dict.get n elementColors
+                                    |> Maybe.withDefault defaultColor
 
-                                WithoutReplacement ->
-                                    "#c8e6c9"
-                            )
-                        , HA.style "border-radius" "3px"
-                        ]
-                        [ Html.text (String.fromInt n) ]
-                )
+                            WithoutReplacement ->
+                                defaultColor
+                        )
+                    , HA.style "border-radius" "3px"
+                    ]
+                    [ Html.text (String.fromInt n) ]
+            )
+            elements
         )
