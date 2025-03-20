c7dc7c684c2456ecdcee0894a89ab1cb8b6779dc
Jan Hrček <honza.hrk@gmail.com>
2025-03-20 15:51:08 +0100

    Bezout 7: support displaying square decomposition

--- bezout/src/Main.elm
+++ bezout/src/Main.elm
@@ -5,7 +5,7 @@ import Browser
 import Browser.Dom as Dom
 import Browser.Events
 import Html exposing (Html)
-import Html.Attributes as HA exposing (style, type_, value)
+import Html.Attributes as HA exposing (style)
 import Html.Events exposing (onInput)
 import Json.Decode as Decode
 import Svg
@@ -31,6 +31,7 @@ type alias Model =
     , a : Int
     , b : Int
     , trace : List EuclidStep
+    , showSquareDecomposition : Bool
     }
 
 
@@ -47,9 +48,10 @@ init _ =
     ( { width = 0
       , height = 0
       , pixelsPerSquare = 10
-      , a = 56
-      , b = 12
-      , trace = euclidTrace 56 12
+      , a = 55
+      , b = 89
+      , trace = euclidTrace 55 89
+      , showSquareDecomposition = False
       }
     , Task.perform GotViewport Dom.getViewport
     )
@@ -64,12 +66,14 @@ type Msg
     | GridClicked Float Float -- mouse coords of the click
     | KeyUpDownPressed Int -- deltaA
     | LeftRightPressed Int -- deltaB
+    | ToggleShowSquareDecomposition
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         WindowResized width height ->
+            -- TODO resizing window should clamp a and b to the new grid size
             pure { model | width = width, height = height }
 
         GotViewport { viewport } ->
@@ -122,6 +126,9 @@ update msg model =
 
         LeftRightPressed delta ->
             pure <| setB (model.b + delta) model
+
+        ToggleShowSquareDecomposition ->
+            pure { model | showSquareDecomposition = not model.showSquareDecomposition }
 
 
 setA : Int -> Model -> Model
@@ -236,40 +243,66 @@ euclidPanel model =
             ]
             [ Html.label [] [ Html.text "a =" ]
             , Html.input
-                [ type_ "number"
-                , value (String.fromInt model.a)
+                [ HA.type_ "number"
+                , HA.value (String.fromInt model.a)
                 , onInput AChanged
                 , style "width" "60px"
                 ]
                 []
             , Html.label [] [ Html.text "b =" ]
             , Html.input
-                [ type_ "number"
-                , value (String.fromInt model.b)
+                [ HA.type_ "number"
+                , HA.value (String.fromInt model.b)
                 , onInput BChanged
                 , style "width" "60px"
                 ]
                 []
             ]
         , renderTrace model.a model.b model.trace
-        , Html.hr [ style "margin" "15px 0", style "border" "0", style "border-top" "1px solid #ccc" ] []
+        , Html.hr
+            [ style "margin" "15px 0"
+            , style "border" "0"
+            , style "border-top" "1px solid #ccc"
+            ]
+            []
         , Html.h4
             [ style "margin" "10px 0" ]
             [ Html.text "Settings" ]
         , Html.div
-            [ style "display" "flex", style "align-items" "center", style "margin-bottom" "15px" ]
+            [ style "display" "flex"
+            , style "align-items" "center"
+            , style "margin-bottom" "15px"
+            ]
             [ Html.label
                 [ style "margin-right" "8px" ]
                 [ Html.text "Pixels per square:" ]
             , Html.input
-                [ type_ "number"
+                [ HA.type_ "number"
                 , HA.min (String.fromInt minPixelsPerSquare)
                 , HA.max (String.fromInt maxPixelsPerSquare)
-                , value (String.fromInt model.pixelsPerSquare)
+                , HA.value (String.fromInt model.pixelsPerSquare)
                 , onInput PixelsPerSquareChanged
                 , style "width" "60px"
                 ]
                 []
+            ]
+        , Html.div
+            [ style "display" "flex"
+            , style "align-items" "center"
+            , style "margin-bottom" "15px"
+            ]
+            [ Html.input
+                [ HA.type_ "checkbox"
+                , HA.checked model.showSquareDecomposition
+                , Html.Events.onClick ToggleShowSquareDecomposition
+                , style "margin-right" "8px"
+                ]
+                []
+            , Html.label
+                [ style "cursor" "pointer"
+                , Html.Events.onClick ToggleShowSquareDecomposition
+                ]
+                [ Html.text "Show steps in grid" ]
             ]
         , Html.div []
             [ Html.p
@@ -393,12 +426,8 @@ renderTrace a b trace =
                 let
                     index =
                         getNumberIndex number
-
-                    colorIndex =
-                        modBy numColors index
                 in
-                Array.get colorIndex colorsArray
-                    |> Maybe.withDefault "black"
+                getColor index
 
             -- Create colored number span
             coloredNumber : Int -> Html Msg
@@ -543,71 +572,117 @@ renderSvgGrid model =
             "0.1"
 
         -- Get color based on Euclid step count (clamped to our color palette)
-        getColor : Int -> Int -> String
-        getColor i j =
+        getStepColor : Int -> Int -> String
+        getStepColor i j =
             let
                 colorIndex =
                     countEuclidSteps i j - 1
             in
-            case Array.get colorIndex colorsArray of
-                Just color ->
-                    color
+            getColor colorIndex
 
-                Nothing ->
-                    "white"
-
+        -- Create the base grid squares
         squares =
-            List.range 1 numRows
-                |> List.concatMap
-                    (\i ->
-                        List.range 1 numCols
-                            |> List.map
-                                (\j ->
-                                    let
-                                        x =
-                                            (j - 1) * model.pixelsPerSquare
+            if model.showSquareDecomposition then
+                List.range 1 numRows
+                    |> List.concatMap
+                        (\i ->
+                            List.range 1 numCols
+                                |> List.concatMap
+                                    (\j ->
+                                        let
+                                            -- When decomposition is enabled, only draw squares outside the a×b rectangle
+                                            isInDecompositionArea =
+                                                i <= model.a && j <= model.b
 
-                                        y =
-                                            (i - 1) * model.pixelsPerSquare
+                                            x =
+                                                (j - 1) * model.pixelsPerSquare
 
-                                        isSelected =
-                                            i == model.a && j == model.b
+                                            y =
+                                                (i - 1) * model.pixelsPerSquare
 
-                                        fillColor =
-                                            if isSelected then
-                                                "#FF5733"
-                                                -- Bright orange for selected cell
+                                            fillColor =
+                                                getStepColor i j
+                                        in
+                                        if isInDecompositionArea then
+                                            -- Skip rendering inside the decomposition area
+                                            -- We'll render the decomposition squares separately
+                                            []
 
-                                            else
-                                                getColor i j
+                                        else
+                                            [ Svg.rect
+                                                [ SA.x (String.fromInt x)
+                                                , SA.y (String.fromInt y)
+                                                , SA.width (String.fromInt model.pixelsPerSquare)
+                                                , SA.height (String.fromInt model.pixelsPerSquare)
+                                                , SA.fill fillColor
+                                                , SA.stroke "black"
+                                                , SA.strokeWidth strokeWidth
+                                                , style "cursor" "pointer"
+                                                ]
+                                                []
+                                            ]
+                                    )
+                        )
 
-                                        strokeColor =
-                                            if isSelected then
-                                                "red"
+            else
+                -- When decomposition is disabled, render the grid normally
+                List.range 1 numRows
+                    |> List.concatMap
+                        (\i ->
+                            List.range 1 numCols
+                                |> List.map
+                                    (\j ->
+                                        let
+                                            x =
+                                                (j - 1) * model.pixelsPerSquare
 
-                                            else
-                                                "black"
+                                            y =
+                                                (i - 1) * model.pixelsPerSquare
 
-                                        strokeWidth_ =
-                                            if isSelected then
-                                                "1.5"
+                                            isSelected =
+                                                i == model.a && j == model.b
 
-                                            else
-                                                strokeWidth
-                                    in
-                                    Svg.rect
-                                        [ SA.x (String.fromInt x)
-                                        , SA.y (String.fromInt y)
-                                        , SA.width (String.fromInt model.pixelsPerSquare)
-                                        , SA.height (String.fromInt model.pixelsPerSquare)
-                                        , SA.fill fillColor
-                                        , SA.stroke strokeColor
-                                        , SA.strokeWidth strokeWidth_
-                                        , style "cursor" "pointer"
-                                        ]
-                                        []
-                                )
-                    )
+                                            fillColor =
+                                                getStepColor i j
+
+                                            strokeColor =
+                                                if isSelected then
+                                                    "red"
+
+                                                else
+                                                    "black"
+
+                                            strokeWidth_ =
+                                                if isSelected then
+                                                    "2"
+
+                                                else
+                                                    strokeWidth
+                                        in
+                                        Svg.rect
+                                            [ SA.x (String.fromInt x)
+                                            , SA.y (String.fromInt y)
+                                            , SA.width (String.fromInt model.pixelsPerSquare)
+                                            , SA.height (String.fromInt model.pixelsPerSquare)
+                                            , SA.fill fillColor
+                                            , SA.stroke strokeColor
+                                            , SA.strokeWidth strokeWidth_
+                                            , style "cursor" "pointer"
+                                            ]
+                                            []
+                                    )
+                        )
+
+        decompositionSquares =
+            if model.showSquareDecomposition && not (List.isEmpty model.trace) then
+                renderDecomposition model.trace model.pixelsPerSquare 0 (model.a > model.b)
+
+            else
+                []
+
+        -- Combine all SVG elements
+        allElements =
+            squares ++ decompositionSquares
     in
     Svg.svg
         [ SA.width (String.fromInt model.width)
@@ -617,7 +692,93 @@ renderSvgGrid model =
         , style "left" "0"
         , SE.on "click" gridClickDecoder
         ]
-        squares
+        allElements
+
+
+{-| Recursively render the square decomposition based on Euclid's algorithm steps
+The isVertical parameter controls stacking direction, alternating with each recursion level
+-}
+renderDecomposition : List EuclidStep -> Int -> Int -> Bool -> List (Svg.Svg Msg)
+renderDecomposition steps pixelsPerSquare level isVertical =
+    case steps of
+        [] ->
+            []
+
+        { b, quotient, remainder } :: remainingSteps ->
+            let
+                strokeWidth =
+                    "0.2"
+
+                sideLen =
+                    b * pixelsPerSquare
+
+                mainSquares =
+                    List.range 0 (quotient - 1)
+                        |> List.map
+                            (\n ->
+                                let
+                                    ( squareX, squareY ) =
+                                        if isVertical then
+                                            ( 0, n * sideLen )
+
+                                        else
+                                            ( n * sideLen, 0 )
+                                in
+                                Svg.rect
+                                    [ SA.x (String.fromInt squareX)
+                                    , SA.y (String.fromInt squareY)
+                                    , SA.width (String.fromInt sideLen)
+                                    , SA.height (String.fromInt sideLen)
+                                    , SA.fill (getColor level)
+                                    , SA.stroke "black"
+                                    , SA.strokeWidth strokeWidth
+                                    ]
+                                    []
+                            )
+
+                -- Calculate the position for the remainder rectangle
+                remainderX =
+                    if isVertical then
+                        0
+
+                    else
+                        quotient * sideLen
+
+                remainderY =
+                    if isVertical then
+                        quotient * sideLen
+
+                    else
+                        0
+
+                -- Translate the recursive decomposition to the remainder position
+                translateSubDecomposition =
+                    if List.isEmpty remainingSteps || remainder == 0 then
+                        []
+
+                    else
+                        -- Apply translation to the recursive elements
+                        -- Switch orientation for next level (alternate between vertical and horizontal)
+                        let
+                            transformAttr =
+                                "translate(" ++ String.fromInt remainderX ++ "," ++ String.fromInt remainderY ++ ")"
+                        in
+                        [ Svg.g
+                            [ SA.transform transformAttr ]
+                            (renderDecomposition remainingSteps pixelsPerSquare (level + 1) (not isVertical))
+                        ]
+            in
+            mainSquares ++ translateSubDecomposition
+
+
+getColor : Int -> String
+getColor i =
+    case Array.get i colorsArray of
+        Just color ->
+            color
+
+        Nothing ->
+            "white"
 
 
 gridClickDecoder : Decode.Decoder Msg
@@ -643,6 +804,8 @@ colors =
     , "slateblue"
     , "mediumpurple"
     , "darkviolet"
+
+    -- TODO add more colors
     ]
 
 
