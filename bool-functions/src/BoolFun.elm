4096ccbac5bc79cefa8ce4bf31701b5924dd50fc
Jan Hrček <honza.hrk@gmail.com>
2025-06-28 16:31:45 +0200

    Boolean functions: initial skeleton

---
+++ bool-functions/src/BoolFun.elm
@@ -0,0 +1,148 @@
+module BoolFun exposing
+    ( F2
+    , evalF2
+    , f2Names
+    , mkF2
+    , truthTableF2
+    )
+
+import Array exposing (Array)
+import Bitwise
+import Html exposing (Html)
+import Html.Attributes as A
+
+
+{-| A compact representation of a function of two Boolean variables
+We interpret the last 4 bits of an integer as a truth table for the function.
+-}
+type F2
+    = F2 Int
+
+
+f2Names : Array String
+f2Names =
+    Array.fromList
+        [ "FALSE"
+        , "NOR"
+        , "¬a ∧ b"
+        , "¬a"
+        , "a ∧ ¬b"
+        , "¬b"
+        , "XOR"
+        , "NAND"
+        , "∧"
+        , "XNOR"
+        , "b"
+        , "a → b"
+        , "b"
+        , "b -> a"
+        , "∨"
+        , "TRUE"
+        ]
+
+
+truthTableF2 : F2 -> Html a
+truthTableF2 ((F2 funIdx) as f2) =
+    Html.table [ A.class "truth-table" ]
+        [ Html.thead []
+            [ Html.tr []
+                [ Html.th [] [ Html.text "a" ]
+                , Html.th [] [ Html.text "b" ]
+                , Html.th [] [ Html.text (Array.get funIdx f2Names |> Maybe.withDefault "f(a, b)") ]
+                ]
+            ]
+        , Html.tbody []
+            (List.range 0 3
+                |> List.map
+                    (\i ->
+                        let
+                            a =
+                                getBit 1 i
+
+                            b =
+                                getBit 0 i
+
+                            result =
+                                evalF2 f2 a b
+                        in
+                        Html.tr []
+                            [ boolCell a
+                            , boolCell b
+                            , boolCell result
+                            ]
+                    )
+            )
+        ]
+
+
+
+-- TODO make it configurable how to display each value (TRUE vs T vs unicode checkmark?)
+
+
+boolCell : Bool -> Html a
+boolCell b =
+    Html.td
+        [ A.style "background-color"
+            (if b then
+                "lightgreen"
+
+             else
+                "lightcoral"
+            )
+        ]
+        [ Html.text (showBool b) ]
+
+
+showBool : Bool -> String
+showBool b =
+    if b then
+        "True"
+
+    else
+        "False"
+
+
+
+-- TODO somehow associate names with each function
+
+
+mkF2 : Int -> Maybe F2
+mkF2 i =
+    -- TODO we could potentially get rid of the Maybe - and just ignore all the bits past the first 4
+    if i < 0 || i > 15 then
+        Nothing
+
+    else
+        Just (F2 i)
+
+
+bitsToIndex : List Bool -> Int
+bitsToIndex bits =
+    List.foldl
+        (\bit acc ->
+            acc
+                * 2
+                + (if bit then
+                    1
+
+                   else
+                    0
+                  )
+        )
+        0
+        bits
+
+
+evalF2 : F2 -> Bool -> Bool -> Bool
+evalF2 (F2 i) a b =
+    -- TODO maybe eval could just take single Int whose last 2 bits would be taken to represent the index of a bit to lookup from F2.
+    getBit (bitsToIndex [ a, b ]) i
+
+
+getBit : Int -> Int -> Bool
+getBit bitIndex n =
+    if bitIndex < 0 || bitIndex > 32 then
+        False
+
+    else
+        Bitwise.and n (Bitwise.shiftLeftBy bitIndex 1) /= 0
---
+++ bool-functions/src/Main.elm
@@ -0,0 +1,273 @@
+module Main exposing (..)
+
+import Array
+import BoolFun
+import Browser
+import Browser.Navigation as Nav
+import Html exposing (Attribute, Html)
+import Html.Attributes as HA
+import Html.Events as Events
+import Url
+import Url.Parser as Parser exposing ((</>), Parser, int, map, oneOf, s, top)
+
+
+main : Program () Model Msg
+main =
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
+        }
+
+
+type alias Model =
+    { key : Nav.Key
+    , url : Url.Url
+    , route : Route
+    }
+
+
+type Route
+    = Home
+    | Functions Int
+    | Function Int Int
+    | NotFound
+
+
+routeParser : Parser (Route -> a) a
+routeParser =
+    oneOf
+        [ map Home top
+        , map Functions (s "functions" </> int)
+        , map Function (s "functions" </> int </> s "function" </> int)
+        ]
+
+
+parseUrl : Url.Url -> Route
+parseUrl url =
+    case url.fragment of
+        Nothing ->
+            Home
+
+        Just fragment ->
+            case Parser.parse routeParser { url | path = fragment } of
+                Just route ->
+                    route
+
+                Nothing ->
+                    NotFound
+
+
+renderRoute : Route -> String
+renderRoute route =
+    "#"
+        ++ (case route of
+                Home ->
+                    ""
+
+                NotFound ->
+                    ""
+
+                Functions arity ->
+                    "/functions/" ++ String.fromInt arity
+
+                Function arity functionIndex ->
+                    "/functions/" ++ String.fromInt arity ++ "/function/" ++ String.fromInt functionIndex
+           )
+
+
+routeHref : Route -> Attribute msg
+routeHref route =
+    HA.href (renderRoute route)
+
+
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init _ url key =
+    ( { key = key
+      , url = url
+      , route = parseUrl url
+      }
+    , Cmd.none
+    )
+
+
+type Msg
+    = LinkClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+    | ArityChanged String
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        UrlChanged url ->
+            ( { model | url = url, route = parseUrl url }
+            , Cmd.none
+            )
+
+        ArityChanged arityStr ->
+            case String.toInt arityStr of
+                Just arity ->
+                    ( model, Nav.pushUrl model.key (renderRoute (Functions arity)) )
+
+                Nothing ->
+                    ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "Function Explorer"
+    , body = [ viewBody model ]
+    }
+
+
+viewBody : Model -> Html Msg
+viewBody model =
+    Html.div []
+        [ Html.node "style" [] [ Html.text styles ]
+        , viewNavigation model.route
+        , viewRoute model.route
+        ]
+
+
+viewNavigation : Route -> Html Msg
+viewNavigation route =
+    Html.div []
+        (List.intersperse (Html.text " > ") (buildBreadcrumbs route))
+
+
+buildBreadcrumbs : Route -> List (Html Msg)
+buildBreadcrumbs route =
+    case route of
+        Home ->
+            [ Html.a [ routeHref Home ] [ Html.text "Home" ] ]
+
+        Functions arity ->
+            [ Html.a [ routeHref Home ] [ Html.text "Home" ]
+            , arityDropdown (Just arity)
+            ]
+
+        Function arity functionIndex ->
+            [ Html.a [ routeHref Home ] [ Html.text "Home" ]
+            , arityDropdown (Just arity)
+            , Html.text ("Function " ++ String.fromInt functionIndex)
+            ]
+
+        NotFound ->
+            [ Html.a [ routeHref Home ] [ Html.text "Home" ] ]
+
+
+arityDropdown : Maybe Int -> Html Msg
+arityDropdown selectedArity =
+    Html.select [ Events.onInput ArityChanged ]
+        (List.map
+            (\arity ->
+                Html.option
+                    [ HA.value (String.fromInt arity)
+                    , HA.selected (selectedArity == Just arity)
+                    ]
+                    [ Html.text ("Arity " ++ String.fromInt arity) ]
+            )
+            [ 1, 2, 3 ]
+        )
+
+
+viewRoute : Route -> Html Msg
+viewRoute route =
+    case route of
+        Home ->
+            Html.text "Home page - Function Explorer"
+
+        Functions arity ->
+            if arity == 2 then
+                Array.toIndexedList BoolFun.f2Names
+                    |> List.map
+                        (\( idx, name ) ->
+                            Html.tr []
+                                [ Html.td [] [ Html.text (String.fromInt idx) ]
+                                , Html.td [] [ Html.a [ routeHref (Function 2 idx) ] [ Html.text name ] ]
+                                ]
+                        )
+                    |> (::)
+                        (Html.thead []
+                            [ Html.tr []
+                                [ Html.th [] [ Html.text "Index" ]
+                                , Html.th [] [ Html.text "Function Name" ]
+                                ]
+                            ]
+                        )
+                    |> Html.table
+                        [ HA.class "functions-table"
+                        ]
+
+            else if 0 < arity && arity < 4 then
+                Html.text ("Functions page for n=" ++ String.fromInt arity)
+
+            else
+                Html.text "Invalid function arity (must be 1-3)"
+
+        Function arity functionIndex ->
+            case arity of
+                1 ->
+                    Html.text "Function with arity 1 - TODO"
+
+                2 ->
+                    case BoolFun.mkF2 functionIndex of
+                        Just f2 ->
+                            BoolFun.truthTableF2 f2
+
+                        Nothing ->
+                            -- TODO add link to meaningful page
+                            Html.text "Invalid function index for arity 2"
+
+                3 ->
+                    Html.text "Function with arity 3 - TODO"
+
+                _ ->
+                    -- TODO add link to meaningful page
+                    Html.text "Invalid function arity (must be 1-3)"
+
+        NotFound ->
+            Html.text "404 - Page not found"
+
+
+styles : String
+styles =
+    """
+.functions-table {
+    border-collapse: collapse;
+    border: 1px solid black;
+    margin: 10px;
+}
+.functions-table th, .functions-table td {
+    border: 1px solid black;
+    padding: 2px;
+}
+
+.truth-table {
+    border-collapse: collapse;
+    border: 1px solid black;
+    margin: 10px;
+}
+.truth-table th, .truth-table td {
+    border: 1px solid black;
+    padding: 2px;
+
+}
+"""
